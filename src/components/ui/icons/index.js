c84f6144ba06a35bc3e5dd342f398373457712d5
Yiroma <yiromaric@gmail.com>
2026-05-06 19:23:38 +0200

    refactor(icons): remplace les icônes SVG par lucide-astro

--- src/components/ui/icons/index.js
+++ src/components/ui/icons/index.js
@@ -3,10 +3,6 @@ export { default as GithubIcon } from "./GithubIcon.astro";
 export { default as LinkedinIcon } from "./LinkedinIcon.astro";
 export { default as LogoIcon } from "./LogoIcon.astro";
 export { default as MailIcon } from "./MailIcon.astro";
-export { default as MenuCloseIcon } from "./MenuCloseIcon.astro";
-export { default as MenuBurgerIcon } from "./MenuBurgerIcon.astro";
-export { default as MoonIcon } from "./MoonIcon.astro";
-export { default as SunIcon } from "./SunIcon.astro";
 
 // TSX icons
 export { GithubIcon as GithubReactIcon } from "./GithubIcon.tsx";
